b7314776fb0885c682b4e79e40ef68cf2fe5d048
Noh Ah Jeong <noh_ah@hotmail.com>
2020-09-28 13:41:54 -0300

    atividade de javascript

---
+++ semana3/variaveis/index.js
@@ -0,0 +1,68 @@
+// Exercícios de interpretação de código
+
+//1.
+/*
+    10
+    10 5
+*/
+
+//2.
+/*
+    10 10 10
+*/
+
+// Exercícios de escrita de código
+
+// 1.
+/*
+    let nome
+    let idade
+    console.log(typeof nome)
+    console.log(typeof idade)
+    // Foi imprimido 'undefined' porque não foi atribuído nenhum valor para as variáveis, portanto o tipo das variáveis não podem ser definidas.
+    nome = prompt("Digite o seu nome")
+    idade = prompt("Digite a sua idade")
+    console.log(typeof nome)
+    console.log(typeof idade)
+    // Foi imprimido 'string' para as duas variáveis, mesmo a idade ser uma variável com números.
+    console.log("Olá "+nome+", você tem "+idade+" anos.")
+*/
+
+//2.
+/*
+    const pergunta1 = prompt("Qual é o seu nome?")
+    console.log("Pergunta: Qual é o seu nome? Resposta:", pergunta1)
+    const pergunta2 = prompt("Onde você mora?")
+    console.log("Pergunta: Onde você mora? Resposta:", pergunta2)
+    const pergunta3 = prompt("Quantos anos você tem?")
+    console.log("Pergunta: Quantos anos você tem? Resposta:", pergunta3)
+    const pergunta4 = prompt("Qual é a sua cor favorita?")
+    console.log("Pergunta: Qual é a sua cor favorita? Resposta:", pergunta4)
+    const pergunta5 = prompt("Qual é o seu time de futebol?")
+    console.log("Pergunta: Qual é o seu time de futebol? Resposta:", pergunta5)
+*/
+
+//3.
+/*
+    let listaDeComida = ['MulNaengMyeon', 'BulGoGi', 'BiBimNaengMyeon', 'JaJangMyeon', 'SunDuBuJjiGae']
+    console.log(listaDeComida)
+
+    console.log("Essas são as minhas comidas preferidas: ")
+    console.log(listaDeComida[0])
+    console.log(listaDeComida[1])
+    console.log(listaDeComida[2])
+    console.log(listaDeComida[3])
+    console.log(listaDeComida[4])
+
+    listaDeComida[1] = prompt("Qual é a sua comida favorita?")
+    console.log(listaDeComida)
+*/
+
+//4.
+/*
+let listaDePerguntas = ['Você gosta do Charmander?', 'Você gosta do Bulbassauro?', 'Você gosta do Squirtle?']
+const listaDeRespostas = [true, true, true]
+console.log(listaDePerguntas[0], listaDeRespostas[0])
+console.log(listaDePerguntas[1], listaDeRespostas[1])
+console.log(listaDePerguntas[2], listaDeRespostas[2])
+*/
