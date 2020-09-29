229b7ba08693b63c0a660f7ed39628500a6ce300
Noh Ah Jeong <noh_ah@hotmail.com>
2020-09-29 14:19:54 -0300

    atividade de operacoes em javascript

---
+++ semana3/operadores/index.js
@@ -0,0 +1,97 @@
+// Exercícios de interpretação de código
+// 1.
+    // a. false
+    // b. false
+    // c. true
+    // e. boolean
+
+// 2.
+    // a. undefined
+    // b. null
+    // c. 11
+    // d. 3
+    // e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    // f. 9
+
+// Exercícios de escrita de código
+// 1.
+    // const idadeDoUsuario = prompt ("Qual é a sua idade?")
+    // const idadeDoAmigo = prompt ("Qual é a idade do seu melhor amigo?")
+    // console.log ("Sua idade é maior do que a do seu melhor amigo?", idadeDoUsuario>idadeDoAmigo)
+    // console.log (Number(idadeDoUsuario) - Number(idadeDoAmigo))
+
+// 2.
+    // const numeroPar = prompt ("Insira um número par")
+    // console.log (Number(numeroPar) % 2)
+    // // O resto da divisão sempre dá 0.
+    // // O resto da divisão dá 1.
+
+// 3.
+    // let listaDeTarefas = []
+    // listaDeTarefas[0] = prompt ("Digite a primeira tarefa de hoje")
+    // listaDeTarefas[1] = prompt ("Digite a segunda tarefa de hoje")
+    // listaDeTarefas[2] = prompt ("Digite a terceira tarefa de hoje")
+    // console.log (listaDeTarefas)
+    // const indice = prompt ("Digite o índice da tarefa que já realizou: 0, 1 ou 2")
+    // listaDeTarefas.splice (indice, 1)
+    // console.log (listaDeTarefas)
+
+// 4.
+    // const nomeDoUsuario = prompt ("Digite o seu nome")
+    // const emailDoUsuario = prompt ("Digite o seu e-mail")
+    // console.log ("O e-mail " + emailDoUsuario + " foi cadastrado com sucesso. Seja bem-vinda(o), " + nomeDoUsuario + "!")
+
+// Desafios Extras
+// 1.a.
+    // console.log ((77 - 32)*5/9 + 273.15 + "K")
+
+// 1.b.
+    // console.log (80*9/5 + 32 + "°F")
+
+// 1.c.
+    // console.log (30*9/5 + 32 + "°F")
+    // console.log (((30*9/5 + 32) - 32)*5/9 + 273.15 + "K")
+
+// 1.d.
+    // let numero = prompt ("Insira a temperatura em °C para converter em °F e K")
+    // console.log (numero*9/5 + 32 + "°F")
+    // console.log (((numero*9/5 + 32) - 32)*5/9 + 273.15 + "K")
+ 
+// 2.a.
+    // let quilowatts = 280
+    // console.log ("R$" + quilowatts * 0.05)
+// 2.b.
+    // quilowatts = prompt ("Insira a quantidade de quilowatt-hora consumida na residência")
+    // let desconto = prompt ("Insira o desconto em % que você recebeu")
+    // desconto = 0.15
+    // desconto = (quilowatts * 0.05) - (quilowatts * 0.05 * desconto)
+    // console.log ("R$" + desconto)
+
+// 3.a.
+    // let X = 20 / 2.20
+    // console.log ("20lb equivalem a " + X + "kg")
+
+// 3.b.
+    // let X = 10.5 / 35.27
+    // console.log ("10.5oz equivalem a " + X + "kg")
+
+// 3.c.
+    // let X = 100 * 1609.34
+    // console.log ("100mi equivalem a " + X + "m")
+
+// 3.d.
+    // let X = 50 / 3.28
+    // console.log ("50ft equivalem a " + X + "m")
+
+// 3.e.
+    // let X = 103.56 * 3.78
+    // console.log ("103.56gal equivalem a " + X + "L")
+
+// 3.f.
+    // let X = 450 / 3.52
+    // console.log ("450xic equivalem a " + X + "L")
+
+// 3.g.
+    // const Y = prompt ("Insira a quantidade de xícaras para converter em litros")
+    // let X = Y / 3.52
+    // console.log (Y + "xic equivalem a " + X + "kg")
