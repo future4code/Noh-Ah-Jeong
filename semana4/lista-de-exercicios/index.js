6bfe118d88fb4e7d951b4fb5ee715d0e6c592230
Noh Ah Jeong <noh_ah@hotmail.com>
2020-10-09 11:22:55 -0300

    exercicio 1 de interpretacao feito

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,8 @@
+// Exercícios de interpretação de código
+/* 1.
+    Através de uma função, é feita uma conversão de um valor em dólar para real.
+    Na função 'conversorDeMoeda', é obtida o valor em dólar a ser convertido por meio de prompt e conversão do mesmo em number na variável 'cotacao'. A função retorna o valor obtido multiplicado pelo input da função 'valorEmDolar'. E a função é invocada e armazenada na variável 'meuDinheiro' com o valor '100' como input.
+    Por fim, é impresso no console o valor do 'meuDinheiro', que é algo do tipo:
+    R$(input do usuário multiplicado por 100)
+*/
+
