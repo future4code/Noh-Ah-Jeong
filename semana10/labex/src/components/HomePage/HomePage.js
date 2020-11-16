955381bdcd1a897748947ed6dd2097946a0b162e
Noh Ah Jeong <noh_ah@hotmail.com>
2020-11-16 17:45:05 -0300

    código inicial

---
+++ semana10/labex/src/components/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+function HomePage() {
+    
+    return (
+        <div>
+            <h1>Bem Vind@ ao LabeX!</h1>
+            <button >Quero me inscrever em viagens!</button>
+            <button >Login</button>
+        </div>
+    );
+}
+
+export default HomePage;
